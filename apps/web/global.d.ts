40e06bc3e823462c7729f1f985fa4f752fe0349a
anarkrypto <anarkrypto@gmail.com>
2024-09-02 19:52:32 -0300

    chore: Add createNullifier function to global declaration file

--- apps/web/global.d.ts
+++ apps/web/global.d.ts
@@ -3,5 +3,6 @@ declare var mina:
       requestAccounts: () => Promise<string[]>;
       getAccounts: () => Promise<string[]>;
       on: (event: "accountsChanged", handler: (event: any) => void) => void;
+      createNullifier: ({ message }: {message: number[]}) => Promise<JsonNullifier>;
     }
   | undefined;
