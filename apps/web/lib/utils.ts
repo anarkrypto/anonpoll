259c8b1237b3bd2fb90a2c6baef8695f185dfedc
anarkrypto <anarkrypto@gmail.com>
2024-09-02 19:59:33 -0300

    refactor: move cn util to it's own file

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -1,10 +1,4 @@
 import { PollProof, PollPublicOutput } from "chain/dist/runtime/modules/poll";
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
 
 export async function mockProof(
   publicOutput: PollPublicOutput,
