8a4ff6b5c8f1f681097c3f821d6cbe1d2067f5b3
anarkrypto <anarkrypto@gmail.com>
2024-09-02 19:57:46 -0300

    feat(util): Add mockProof function for generating dummy poll proofs

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -1,6 +1,19 @@
+import { PollProof, PollPublicOutput } from "chain/dist/runtime/modules/poll";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
  
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export async function mockProof(
+  publicOutput: PollPublicOutput,
+): Promise<PollProof> {
+  const dummy = await PollProof.dummy([], [""], 2);
+  return new PollProof({
+    proof: dummy.proof,
+    maxProofsVerified: 2,
+    publicInput: undefined,
+    publicOutput,
+  });
 }
