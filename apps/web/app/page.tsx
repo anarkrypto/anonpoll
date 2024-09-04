3f4aace56344da65417c9fe71c8c53582eab72d1
anarkrypto <anarkrypto@gmail.com>
2024-09-04 00:12:43 -0300

    refactor: Remove async-page-dynamic

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,26 @@
 "use client";
+
 import "reflect-metadata";
-import AsyncPageDynamic from "@/containers/async-page-dynamic";
+
+import { Poll } from "@/components/poll";
+import { useObservePoll, usePoll } from "@/lib/stores/poll";
+import { useWalletStore } from "@/lib/stores/wallet";
 
 export default function Home() {
-  return <AsyncPageDynamic />;
+  const wallet = useWalletStore();
+
+  useObservePoll(1);
+  const { vote, votes, loading } = usePoll(1);
+
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center p-4 md:p-8">
+      <Poll
+        wallet={wallet.wallet}
+        onConnectWallet={wallet.connectWallet}
+        onVote={vote}
+        loading={loading}
+        votes={votes}
+      />
+    </div>
+  );
 }
