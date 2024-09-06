a0cb038580922f7e04b5835d326711491f4aaf67
anarkrypto <anarkrypto@gmail.com>
2024-09-06 00:04:54 -0300

    feat: add url utils

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -10,9 +10,14 @@ export const mockProof = async (
     publicInput: undefined,
     publicOutput,
   });
-}
+};
 
-export const truncateMiddle = (str: string, start: number, end: number, separator: string = "...") => {
+export const truncateMiddle = (
+  str: string,
+  start: number,
+  end: number,
+  separator: string = "...",
+) => {
   if (str.length <= start + end) {
     return str;
   }
@@ -21,4 +26,31 @@ export const truncateMiddle = (str: string, start: number, end: number, separato
 
 export const truncateWalletAddress = (address: string) => {
   return truncateMiddle(address, 7, 7);
+};
+
+/**
+ * Ensures that the URL starts with http:// or https://.
+ * @param url
+ * @returns
+ */
+export const ensureHttpOrHttps = (url: string) => {
+  if (!url.startsWith("http://") && !url.startsWith("https://")) {
+    return `https://${url}`;
+  }
+  return url;
+};
+
+/**
+ * Gets the site URL based on environment variables or defaults to localhost.
+ * @returns {string} The origin URL.
+ */
+export const getSiteUrl = () => {
+  let url =
+    process.env.NEXT_PUBLIC_SITE_URL || // Set this to your site URL in production env.
+    process.env.NEXT_PUBLIC_VERCEL_URL || // Deploy URL automatically set by Vercel.
+    "http://localhost:3000"; // Default to localhost.
+
+  url = ensureHttpOrHttps(url);
+
+  return new URL(url).origin;
 };
