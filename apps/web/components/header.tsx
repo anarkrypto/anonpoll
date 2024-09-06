c2727ed4c7112ecc4c4b07168db7da127fd9de16
anarkrypto <anarkrypto@gmail.com>
2024-09-06 01:06:49 -0300

    refactor: implement auth store in header

--- apps/web/components/header.tsx
+++ apps/web/components/header.tsx
@@ -4,8 +4,10 @@ import { Chain } from "./chain";
 import { Separator } from "./ui/separator";
 import { Montserrat } from "next/font/google";
 import { cn } from "@/lib/cn";
-import { useChain } from "@/hooks/useChain";
 import { truncateWalletAddress } from "@/lib/utils";
+import { useChainStore } from "@/lib/stores/chain";
+import { useAuthStore } from "@/lib/stores/auth";
+import { useWalletStore } from "@/lib/stores/wallet";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -22,30 +24,34 @@ export interface HeaderProps {
 }
 
 export default function Header() {
+  const blockHeight = useChainStore((state) => state.block?.height);
+  const {wallet, loading: loadingWallet} = useWalletStore();
+  const { isAuthenticated, loading: loadingAuth, authenticate } = useAuthStore();
 
-  const { client, chain, wallet } = useChain();
+  const loading = loadingWallet || loadingAuth;
+  const showWallet = isAuthenticated && wallet
 
   return (
-    <header className="flex justify-center border-b p-2 shadow-sm">
-      <div className="w-full max-w-7xl flex justify-between items-center">
+    <header className="flex justify-center border-b bg-white p-2 shadow-sm">
+      <div className="flex w-full max-w-7xl items-center justify-between">
         <div className="flex items-center">
-          <h1 className={cn(montserrat.className, 'text-xl font-semibold')}>Zero<span className="text-primary font-bold">Poll</span></h1>
+          <h1 className={cn(montserrat.className, "text-xl font-semibold")}>
+            Zero<span className="font-bold text-primary">Poll</span>
+          </h1>
           <Separator className="mx-4 h-8" orientation={"vertical"} />
           <div className="flex-grow">
-            <Chain height={chain.block?.height ?? "-"} />
+            <Chain height={blockHeight ?? "-"} />
           </div>
         </div>
         <div>
           {/* wallet */}
-          <Button loading={client.loading} onClick={wallet.connectWallet}>
-            {wallet.wallet ? (
+          <Button loading={loading} onClick={authenticate}>
+            {showWallet ? (
               <div className="text-xs sm:text-sm">
-                {truncateWalletAddress(wallet.wallet)}
+                {truncateWalletAddress(wallet)}
               </div>
             ) : (
-              <div className="text-sm">
-                Connect wallet
-              </div>
+              <div className="text-sm">Connect wallet</div>
             )}
           </Button>
         </div>
