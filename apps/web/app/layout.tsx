421f43d850f0108b9fd60600418a9b93c10543e7
anarkrypto <anarkrypto@gmail.com>
2024-09-06 01:07:54 -0300

    refactor: move init code to RootLayout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -6,12 +6,13 @@ import { useEffect } from "react";
 
 import { usePollBlockHeight } from "@/lib/stores/chain";
 import { useObserveBalance } from "@/lib/stores/balances";
-import { useNotifyTransactions } from "@/lib/stores/wallet";
-import { useChain } from "@/hooks/useChain";
+import { useNotifyTransactions, useWalletStore } from "@/lib/stores/wallet";
 
 import Header from "@/components/header";
 import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/cn";
+import { useClientStore } from "@/lib/stores/client";
+import { useAuthStore } from "@/lib/stores/auth";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
@@ -24,20 +25,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
 
+  const wallet = useWalletStore();
+  const startClient = useClientStore((state) => state.start);
+  const verifyAuth = useAuthStore((state) => state.verifyAuth);
+
   usePollBlockHeight();
   useObserveBalance();
   useNotifyTransactions();
-  const { init } = useChain();
 
   useEffect(() => {
-    init();
+    // init on mount
+    startClient();
+    wallet.initializeWallet();
+    wallet.observeWalletChange();
+    verifyAuth();
   }, []);
 
   return (
     <html lang="en" className="h-full">
       <body
         className={cn(
-          "flex min-h-full flex-col bg-background font-sans antialiased",
+          "flex min-h-full flex-col bg-gradient-to-b from-purple-50 to-background font-sans antialiased",
           fontSans.variable,
         )}
       >
