1a454f17219ba668b5a00eac653de2f392f39bd8
anarkrypto <anarkrypto@gmail.com>
2024-09-07 03:15:26 -0300

    feat: add util isValidPublicKey

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -1,4 +1,5 @@
 import { PollProof, PollPublicOutput } from "chain/dist/runtime/modules/poll";
+import { PublicKey } from "o1js";
 
 export const mockProof = async (
   publicOutput: PollPublicOutput,
@@ -26,6 +27,15 @@ export const truncateMiddle = (
 
 export const truncateWalletAddress = (address: string) => {
   return truncateMiddle(address, 7, 7);
+};
+
+export const isValidPublicKey = (publicKey: string) => {
+  try {
+    PublicKey.fromBase58(publicKey);
+    return true;
+  } catch (error) {
+    return false;
+  }
 };
 
 /**
