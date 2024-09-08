1dbbf19cc92ea63e468cccb3b1ed344b66884f72
anarkrypto <anarkrypto@gmail.com>
2024-09-08 03:05:11 -0300

    feat: add loading skeleton page for poll

---
+++ apps/web/app/polls/[pollId]/loading.tsx
@@ -0,0 +1,26 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function PollCardSkeleton() {
+  return (
+    <Card className="w-full max-w-xl p-4">
+      <CardHeader>
+        <Skeleton className="h-8 w-3/4" />
+        <Skeleton className="h-4 w-1/2" />
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <Skeleton className="h-10 w-full" />
+        <Skeleton className="h-10 w-full" />
+        <Skeleton className="h-10 w-full" />
+      </CardContent>
+      <CardFooter>
+        <Skeleton className="h-8 w-48" />
+      </CardFooter>
+    </Card>
+  );
+}
