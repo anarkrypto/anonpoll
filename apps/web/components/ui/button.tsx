d4b8c54e1d4dc17f56e6e9a34f0e99247ff7b213
anarkrypto <anarkrypto@gmail.com>
2024-10-09 23:25:42 -0300

    style: improve button loading style

--- apps/web/components/ui/button.tsx
+++ apps/web/components/ui/button.tsx
@@ -62,8 +62,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
         disabled={loading || props.disabled}
       >
-        {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-        {children}
+        {loading ? <Loader2 className="h-4 w-4 animate-spin" /> : children}
       </Comp>
     );
   },
