bd7ba94949db8b4be820efe1d64b72a2cd60a9d2
anarkrypto <anarkrypto@gmail.com>
2024-10-15 21:58:02 -0300

    feat: add wallet-controller

---
+++ apps/web/lib/core/controllers/wallet-controller.ts
@@ -0,0 +1,46 @@
+import { BaseController, Store } from "./base-controller";
+import { MinaProvider, MinaProviderError } from "../providers/base-provider";
+
+type WalletState = {
+  account: string | null;
+  loading: boolean;
+};
+
+export class WalletController extends BaseController<WalletState> {
+  provider: MinaProvider;
+
+  constructor(provider: MinaProvider, store: Store<WalletState>) {
+    super(store);
+    this.provider = provider;
+  }
+
+  public async init() {
+    this.store.setState({ loading: true });
+
+    try {
+      const [account] = await this.provider.getAccounts();
+
+      this.store.setState({ account });
+    } catch (error) {
+      throw MinaProviderError.fromJson(error);
+    } finally {
+      this.store.setState({ loading: false });
+    }
+  }
+
+  public async connect() {
+    this.store.setState({ loading: true });
+
+    try {
+      const [account] = await this.provider.requestAccounts();
+
+      this.store.setState({ account });
+    } catch (error) {
+      throw MinaProviderError.fromJson(error);
+    } finally {
+      this.store.setState({ loading: false });
+    }
+  }
+}
+
+
