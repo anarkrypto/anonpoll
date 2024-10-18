f081b73402b2bca817f1fcb49bd237a775b374c8
anarkrypto <anarkrypto@gmail.com>
2024-10-17 21:45:16 -0300

    Refactor BaseController to improve state management and listener functionality

--- apps/web/core/controllers/base-controller.ts
+++ apps/web/core/controllers/base-controller.ts
@@ -1,21 +1,79 @@
 import { Logger } from "../utils/logger";
 
+/**
+ * @type BaseState
+ *
+ * Base state representation
+ */
 export type BaseState = Record<string, any>;
 
-export type Store<State extends BaseState> = {
-  getState: () => State;
-  setState: (state: Partial<State>) => void;
-  subscribe: (listener: () => void) => () => void;
-};
+/**
+ * @type Listener
+ *
+ * State change callbacks
+ */
+export type Listener<State extends BaseState> = (state: State) => void;
 
 export class BaseController<State extends BaseState> {
-  protected state: State;
-  protected store: Store<State>;
+
+  readonly defaultState: State = {} as State;
+  private internalState: State;
+  private internalListeners = new Set<Listener<State>>();
   protected logger: Logger;
 
-  constructor(store: Store<State>) {
-    this.store = store;
-    this.logger = new Logger({ isLocalDev: process.env.NODE_ENV === "development" });
-    this.state = {} as State;
+  constructor(initialState: Partial<State> = {} as State) {
+    this.logger = new Logger({
+      isLocalDev: process.env.NODE_ENV === "development",
+    });
+    this.internalState = this.defaultState;
+    this.update(initialState);
+  }
+
+  /**
+   * Retrieves current controller state.
+   *
+   * @returns The current state.
+   */
+  get state() {
+    return this.internalState;
+  }
+
+  /**
+   * Updates controller state.
+   *
+   * @param state - The new state.
+   * @param overwrite - Overwrite state instead of merging.
+   */
+  update(state: Partial<State>) {
+    this.internalState = Object.assign({}, this.internalState, state);
+    this.notify(this.internalState);
+  }
+
+  /**
+   * Notifies all subscribed listeners of current modified state.
+   */
+  private notify(state: State) {
+    this.internalListeners.forEach((listener) => {
+      listener(state);
+    });
+  }
+
+  /**
+   * Adds new listener to be notified of state changes.
+   *
+   * @param listener - The callback triggered when state changes.
+   */
+  subscribe(listener: () => void) {
+    this.internalListeners.add(listener);
+  }
+
+  /**
+   * Removes existing listener from receiving state changes.
+   *
+   * @param listener - The callback to remove.
+   * @returns `true` if a listener is found and unsubscribed.
+   */
+  unsubscribe(listener: Listener<State>) {
+    return this.internalListeners.delete(listener);
   }
 }
