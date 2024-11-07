528c169eacdd15c4b715720775e3450563ab4ca4
anarkrypto <anarkrypto@gmail.com>
2024-11-07 05:37:00 -0300

    refactor: remove log

--- apps/web/core/context-provider.tsx
+++ apps/web/core/context-provider.tsx
@@ -35,8 +35,6 @@ export function ZeroPollProvider({ children }: { children: React.ReactNode }) {
     engine.context.pollManager.state,
   );
 
-  console.log("here");
-
   useEffect(() => {
     engine.context.chain.start();
 
