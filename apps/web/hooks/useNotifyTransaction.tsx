4ace86b4b448f5f59e2f1acf4c4ad1889a5be876
anarkrypto <anarkrypto@gmail.com>
2024-11-11 09:32:19 -0300

    feat: add useNotifyTransactions hook

---
+++ apps/web/hooks/useNotifyTransaction.tsx
@@ -0,0 +1,48 @@
+import { toast } from "@/components/ui/use-toast";
+import { TransactionJSON } from "@/core/controllers/wallet-controller";
+import { useWallet } from "@/core/hooks";
+import { truncateMiddle } from "@/lib/utils";
+import { useEffect, useRef } from "react";
+
+export const notifyTransaction = (transaction: TransactionJSON) => {
+  const hash = truncateMiddle(transaction.hash.toString(), 15, 15, "...");
+
+  function title() {
+    switch (transaction.status) {
+      case "PENDING":
+        return `⏳ Transaction sent: ${transaction.methodModule}.${transaction.methodName}`;
+      case "SUCCESS":
+        return `✅ Transaction successful: ${transaction.methodModule}.${transaction.methodName}`;
+      case "FAILURE":
+        return `❌ Transaction failed: ${transaction.methodModule}.${transaction.methodName}`;
+    }
+  }
+
+  toast({
+    title: title(),
+    description: (
+      <div>
+        {transaction.statusMessage && (
+          <p>
+            <b>Message:</b> {transaction.statusMessage}
+          </p>
+        )}
+        <p>
+          <b>Hash</b>: {hash}
+        </p>
+      </div>
+    ),
+  });
+};
+
+export const useNotifyTransactions = () => {
+  const notifiedTransactions = useRef<TransactionJSON[]>([]);
+  const { transactions } = useWallet();
+  useEffect(() => {
+    transactions.forEach((transaction) => {
+      if (notifiedTransactions.current.includes(transaction)) return;
+      notifyTransaction(transaction);
+      notifiedTransactions.current.push(transaction);
+    });
+  }, [transactions]);
+};
