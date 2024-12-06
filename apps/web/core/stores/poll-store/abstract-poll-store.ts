b23aa3a1d877d3644c108b744ef8c48e8c8e0053
anarkrypto <anarkrypto@gmail.com>
2024-12-06 19:24:36 -0300

    refactor: update abstract poll store methods to use CID instead of numeric pollId

--- apps/web/core/stores/poll-store/abstract-poll-store.ts
+++ apps/web/core/stores/poll-store/abstract-poll-store.ts
@@ -3,6 +3,6 @@ import { PollData } from "@/types/poll";
 import { z } from "zod";
 
 export abstract class AbstractPollStore {
-  abstract getById(pollId: number): Promise<PollData>;
+  abstract get(cid: string): Promise<PollData>;
   abstract persist(data: z.infer<typeof pollInsertSchema>): Promise<void>;
 }
