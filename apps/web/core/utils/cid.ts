89c6be29a3fa4b7e08b6893b6c388cc9b34bd1a6
anarkrypto <anarkrypto@gmail.com>
2024-12-09 17:49:32 -0300

    feat: add utility function to validate CID format

---
+++ apps/web/core/utils/cid.ts
@@ -0,0 +1,10 @@
+import { CID } from "multiformats";
+
+export const isCID = (value: string): boolean => {
+    try {
+        CID.parse(value);
+        return true;
+    } catch {
+        return false;
+    }
+}
