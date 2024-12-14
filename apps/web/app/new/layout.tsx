d3b5b73bfa57a90d540d4878abdafb862bb300b8
anarkrypto <anarkrypto@gmail.com>
2024-12-14 00:18:47 -0300

    refactor: move metadata definition to layout component for New Poll page

--- apps/web/app/new/layout.tsx
+++ apps/web/app/new/layout.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "New Poll",
+}
+
 export default function NewPollLayout({
   children,
 }: {
--- apps/web/app/new/page.tsx
+++ apps/web/app/new/page.tsx
@@ -1,9 +1,4 @@
-import { Metadata } from "next";
 import { PollForm } from "./poll-form";
-
-export const metadata: Metadata = {
-    title: "New Poll",
-}
 
 export default function PollFormPage () {
     return (<PollForm />);
