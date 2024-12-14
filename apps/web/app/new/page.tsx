6ba3a2e0846a549301561b7a465903fe6be32572
anarkrypto <anarkrypto@gmail.com>
2024-12-14 00:19:55 -0300

    refactor: replace PollForm with PollFormCard and integrate ConnectWalletModal for wallet connection

--- apps/web/app/new/page.tsx
+++ apps/web/app/new/page.tsx
@@ -1,5 +1,19 @@
-import { PollForm } from "./poll-form";
+"use client";
 
-export default function PollFormPage () {
-    return (<PollForm />);
+import { useWallet } from "@/core/hooks";
+import { PollFormCard } from "./poll-form-card";
+import { ConnectWalletModal } from "@/components/connect-wallet-modal";
+import { cn } from "@/lib/cn";
+
+export default function PollFormPage() {
+  const { connected, initialized } = useWallet();
+
+  const openConnectWalletModal = initialized && !connected;
+
+  return (
+    <>
+    <PollFormCard className={cn(openConnectWalletModal && "blur-sm")} />
+    <ConnectWalletModal open={openConnectWalletModal} />
+    </>
+  )
 }
