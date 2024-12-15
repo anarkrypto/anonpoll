bf4b027fe06288fdf3b65de834962b8508d9bcf5
anarkrypto <anarkrypto@gmail.com>
2024-12-15 03:18:46 -0300

    feat: add "prettier-config" package for consistent code formatting

---
+++ packages/prettier-config/index.js
@@ -0,0 +1,21 @@
+/**
+ * @type {import('prettier').Options}
+ */
+
+const config = {
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  bracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: true
+};
+
+export default config;
