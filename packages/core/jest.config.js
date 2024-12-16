06777699d893b0c4ec47d4e44fedc4b4c92b5c70
anarkrypto <anarkrypto@gmail.com>
2024-12-16 03:40:29 -0300

    feat: add Jest configuration for core package

---
+++ packages/core/jest.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/unambiguous */
+/* eslint-disable import/no-commonjs */
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+
+const baseConfig = require('chain/jest.config.cjs');
+
+module.exports = {
+	...baseConfig,
+	setupFilesAfterEnv: [],
+	moduleNameMapper: {
+		...baseConfig.moduleNameMapper,
+		'^@/(.*)$': '<rootDir>/src/$1',
+	},
+};
