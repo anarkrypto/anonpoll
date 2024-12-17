134088e4a529ec6747f4da1497a6e99f5e05569e
anarkrypto <anarkrypto@gmail.com>
2024-12-16 23:17:14 -0300

    chore: update prettier configuration to enforce semicolons

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -3,19 +3,19 @@
  */
 
 const config = {
-  arrowParens: "avoid",
-  bracketSpacing: true,
-  endOfLine: "lf",
-  bracketSameLine: false,
-  jsxSingleQuote: false,
-  printWidth: 80,
-  proseWrap: "preserve",
-  quoteProps: "as-needed",
-  semi: false,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: "es5",
-  useTabs: true
+	arrowParens: "avoid",
+	bracketSpacing: true,
+	endOfLine: "lf",
+	bracketSameLine: false,
+	jsxSingleQuote: false,
+	printWidth: 80,
+	proseWrap: "preserve",
+	quoteProps: "as-needed",
+	semi: true,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: "es5",
+	useTabs: true
 };
 
 export default config;
