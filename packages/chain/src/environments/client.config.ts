07d7a4039d74bd4c362e9e67cf6d001674ba48e8
anarkrypto <anarkrypto@gmail.com>
2024-12-20 11:30:45 -0300

    fix(chain): register ProvableTransactionHook and ProvableBlockHook to resolve dependency injection errors Read more: https://discord.com/channels/484437221055922177/1319673057849380995

--- packages/chain/src/environments/client.config.ts
+++ packages/chain/src/environments/client.config.ts
@@ -1,16 +1,27 @@
 import { AuroSigner, ClientAppChain } from "@proto-kit/sdk";
 import runtime from "../runtime";
+import { container } from "tsyringe";
+import {
+	ProvableBlockHook,
+	ProvableTransactionHook
+} from "@proto-kit/protocol";
 
 const appChain = ClientAppChain.fromRuntime(runtime.modules, AuroSigner);
 
 appChain.configurePartial({
-  Runtime: runtime.config,
+	Runtime: runtime.config
 });
 
 appChain.configurePartial({
-  GraphqlClient: {
-    url: process.env.NEXT_PUBLIC_PROTOKIT_GRAPHQL_URL,
-  },
+	GraphqlClient: {
+		url: process.env.NEXT_PUBLIC_PROTOKIT_GRAPHQL_URL
+	}
 });
 
 export const client = appChain;
+
+// This is a workaround for the following error:
+// Uncaught (in promise) Error: Cannot inject the dependency "n" at position #2 of "eX" constructor. Reason:
+//     Attempted to resolve unregistered dependency token: "ProvableTransactionHook" | "ProvableBlockHook"
+container.registerInstance("ProvableTransactionHook", ProvableTransactionHook);
+container.registerInstance("ProvableBlockHook", ProvableBlockHook);
