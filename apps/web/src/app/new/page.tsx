ff01f4def9cad9adbebef041baf2de4066bb8b6e
anarkrypto <anarkrypto@gmail.com>
2024-12-20 00:10:59 -0300

    refactor: move PollFormPage success and error handling for poll creation

--- apps/web/src/app/new/page.tsx
+++ apps/web/src/app/new/page.tsx
@@ -1,18 +1,39 @@
 'use client';
 
-import { useWallet } from '@zeropoll/react';
+import { CreatePollResult, useWallet } from '@zeropoll/react';
 import { PollFormCard } from '@/components/poll-form-card';
 import { ConnectWalletModal } from '@/components/connect-wallet-modal';
 import { cn } from '@/lib/cn';
+import { useRouter } from 'next/router';
+import { useToast } from '@/components/ui/use-toast';
 
 export default function PollFormPage() {
+	const router = useRouter();
+	const { toast } = useToast();
+
 	const { connected, initialized } = useWallet();
 
 	const openConnectWalletModal = initialized && !connected;
 
+	const onSuccess = ({ id, encryptionKey }: CreatePollResult) => {
+		router.push(`/polls/${id}?key=${encryptionKey}`);
+	};
+
+	const onError = (message: string) => {
+		toast({
+			title: 'Error',
+			description: message,
+			variant: 'destructive',
+		});
+	};
+
 	return (
 		<>
-			<PollFormCard className={cn(openConnectWalletModal && 'blur-sm')} />
+			<PollFormCard
+				className={cn(openConnectWalletModal && 'blur-sm')}
+				onSuccess={onSuccess}
+				onError={onError}
+			/>
 			<ConnectWalletModal open={openConnectWalletModal} />
 		</>
 	);
