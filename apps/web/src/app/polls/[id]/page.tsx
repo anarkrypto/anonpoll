6f702ec0a7d1499e597fa9cc2d53e7ee0439129c
anarkrypto <anarkrypto@gmail.com>
2024-12-28 23:44:38 -0300

    feat(web): add error handling for voting in PollCard component

--- apps/web/src/app/polls/[id]/page.tsx
+++ apps/web/src/app/polls/[id]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useToast } from '@/components/ui/use-toast';
 import { PollCard } from '@zeropoll/react-ui';
 import { use } from 'react';
 
@@ -7,8 +8,22 @@ export default function PollPage(props: {
 	params: Promise<{ id: string }>;
 	searchParams: Promise<{ key?: string }>;
 }) {
+	const { toast } = useToast();
+
 	const searchParams = use(props.searchParams);
 	const params = use(props.params);
 
-	return <PollCard id={params.id} encryptionKey={searchParams.key} />;
+	return (
+		<PollCard
+			id={params.id}
+			encryptionKey={searchParams.key}
+			onVoteError={message => {
+				toast({
+					title: 'Error Voting',
+					description: message,
+					variant: 'destructive',
+				});
+			}}
+		/>
+	);
 }
