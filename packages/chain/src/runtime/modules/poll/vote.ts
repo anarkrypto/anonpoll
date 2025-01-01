d7362990e10299c2183b7b52679645ff2f547bc2
anarkrypto <anarkrypto@gmail.com>
2025-01-01 18:45:08 -0300

    refactor(chain): reorganize poll module structure and separate vote logic

--- packages/chain/src/runtime/modules/poll/vote.ts
+++ packages/chain/src/runtime/modules/poll/vote.ts
@@ -1,16 +1,5 @@
-/*
-	NOTE: The proof verification has known vulnerabilities while Protokit is in development
-	and snarky is not implemented yet.
-*/
-
 import { UInt32 } from "@proto-kit/library";
-import {
-	runtimeModule,
-	state,
-	runtimeMethod,
-	RuntimeModule
-} from "@proto-kit/module";
-import { StateMap, assert } from "@proto-kit/protocol";
+import { assert } from "@proto-kit/protocol";
 import {
 	Field,
 	Bool,
@@ -93,11 +82,6 @@ export class Votes extends Struct({
 	}
 }
 
-export class PollStruct extends Struct({
-	votersRoot: Field,
-	votes: Votes
-}) {}
-
 export class VotePublicInputs extends Struct({
 	pollId: CircuitString,
 	optionHash: Field,
@@ -147,63 +131,3 @@ export const voteProgram = ZkProgram({
 });
 
 export class VoteProof extends ZkProgram.Proof(voteProgram) {}
-
-@runtimeModule()
-export class Poll extends RuntimeModule {
-	@state() public polls = StateMap.from<CircuitString, PollStruct>(
-		CircuitString,
-		PollStruct
-	);
-	@state() public nullifiers = StateMap.from<Field, Bool>(Field, Bool);
-
-	@runtimeMethod()
-	public async createPoll(
-		id: CircuitString,
-		votersRoot: Field,
-		optionsHashes: OptionsHashes
-	) {
-		const checkPoll = await this.polls.get(id);
-		assert(checkPoll.isSome.not(), "Poll already exists");
-		await this.polls.set(
-			id,
-			new PollStruct({
-				votersRoot,
-				votes: Votes.fromOptionsHashes(optionsHashes)
-			})
-		);
-	}
-
-	@runtimeMethod()
-	public async vote(proof: VoteProof) {
-		const pollId = proof.publicInput.pollId;
-
-		proof.verify();
-
-		const poll = await this.polls.get(pollId);
-
-		assert(poll.isSome, "Poll does not exist");
-
-		assert(
-			proof.publicInput.votersRoot.equals(poll.value.votersRoot),
-			"Poll proof does not contain the correct votersRoot"
-		);
-
-		const isNullifierUsed = await this.nullifiers.get(
-			proof.publicOutput.nullifier
-		);
-
-		assert(isNullifierUsed.value.not(), "Nullifier has already been used");
-
-		await this.nullifiers.set(proof.publicOutput.nullifier, Bool(true));
-
-		const currentVotes = await poll.value.votes;
-
-		await this.polls.set(
-			pollId,
-			new PollStruct({
-				votersRoot: poll.value.votersRoot,
-				votes: Votes.cast(currentVotes, proof.publicOutput.optionHash)
-			})
-		);
-	}
-}
