39d4f27242349113982d93c117a818f7c12fcbad
anarkrypto <anarkrypto@gmail.com>
2025-01-16 18:51:10 -0300

    feat: add "Made with" section

--- apps/web/src/app/(landing)/page.tsx
+++ apps/web/src/app/(landing)/page.tsx
@@ -30,25 +30,70 @@ export default function LandingPage() {
 					</Link>
 				</div>
 
-				{/* Features Section */}
-				<div className="mt-10 grid grid-cols-1 gap-8 sm:mt-20 md:grid-cols-3">
-					<FeatureCard
-						Icon={Shield}
-						title="Secure"
-						description="Your vote is protected by state-of-the-art cryptography."
-					/>
-					<FeatureCard
-						Icon={Lock}
-						title="Private"
-						description="Your identity remains anonymous throughout the voting process."
-					/>
-					<FeatureCard
-						Icon={CheckSquare}
-						title="Verifiable"
-						description="Ensure the integrity of the voting results without compromising privacy."
-					/>
+			{/* Features Section */}
+			<section className="grid grid-cols-1 gap-8 md:grid-cols-3">
+				<FeatureCard
+					Icon={Shield}
+					title="Secure"
+					description="Your vote is protected by state-of-the-art cryptography."
+				/>
+				<FeatureCard
+					Icon={Lock}
+					title="Private"
+					description="Your identity remains anonymous throught the voting process."
+				/>
+				<FeatureCard
+					Icon={CheckSquare}
+					title="Verifiable"
+					description="Ensure the integrity of the voting results without compromising privacy."
+				/>
+			</section>
+
+			{/* Made with by Section */}
+			<section className="flex flex-1 flex-col items-center justify-center">
+				<h3 className="mb-2 text-center text-base text-zinc-400">Made with</h3>
+				<div className="align-center flex w-fit flex-row flex-wrap items-center justify-center gap-12">
+					{madeWithList.map((item, index) => (
+						<a href={item.url} target="_blank" key={index}>
+							<img
+								alt={item.name}
+								className="mx-auto w-auto text-center opacity-60 grayscale hover:opacity-100 hover:grayscale-0"
+								src={item.icon}
+								style={{
+									height: item.height,
+								}}
+							/>
+						</a>
+					))}
 				</div>
-			</div>
+			</section>
 		</main>
 	);
 }
+
+const madeWithList = [
+	{
+		name: 'Mina Protocol',
+		url: 'https://minaprotocol.com',
+		icon: '/assets/mina.svg',
+		height: 28,
+	},
+	{
+		name: 'Protokit',
+		url: 'https://protokit.dev',
+		icon: '/assets/protokit.svg',
+		height: 28,
+	},
+	{
+		name: 'o1js',
+		url: 'https://docs.minaprotocol.com/zkapps/o1js',
+		icon: '/assets/o1js.svg',
+		height: 40,
+	},
+	{
+		name: 'IPFS',
+		url: 'https://ipfs.tech',
+		icon: '/assets/ipfs.png',
+		height: 28,
+	},
+];
