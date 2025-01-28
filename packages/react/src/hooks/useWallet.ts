df909abf2f8055df3e6b086075295fdb873f53a4
anarkrypto <anarkrypto@gmail.com>
2025-01-28 10:28:13 -0300

    feat: add isInstalled state to useWallet hook to check wallet installation status

--- packages/react/src/hooks/useWallet.ts
+++ packages/react/src/hooks/useWallet.ts
@@ -1,15 +1,22 @@
 'use client';
 
 import { WalletState } from '@zeropoll/core/controllers';
-import { useCallback, useSyncExternalStore } from 'react';
+import { useCallback, useEffect, useState, useSyncExternalStore } from 'react';
 import { useZeroPoll } from '../zeropoll-provider';
+import { AuroWallet } from '@zeropoll/core/signers';
 
 export interface UseWalletReturn extends WalletState {
 	connect: () => Promise<void>;
+	isInstalled: boolean;
 }
 
 export const useWallet = (): UseWalletReturn => {
 	const { zeroPoll } = useZeroPoll();
+	const [isInstalled, setIsInstalled] = useState(false);
+
+	useEffect(() => {
+		setIsInstalled(AuroWallet.isInstalled());
+	}, []);
 
 	const state = useSyncExternalStore(
 		callback => zeroPoll.wallet.subscribe(callback),
@@ -19,6 +26,7 @@ export const useWallet = (): UseWalletReturn => {
 
 	return {
 		...state,
+		isInstalled,
 		connect: useCallback(() => zeroPoll.wallet.connect(), [zeroPoll.wallet]),
 	};
 };
